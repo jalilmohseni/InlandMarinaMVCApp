baf48bb17968e00bc73e504265349b48db8d6488
jalil1984 <jalil.mohseni@gmail.com>
2024-11-29 22:12:29 -0700

    Add project files.

---
+++ InlandMarinaData/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace InlandMarinaData
+{
+    [Table("Customer")]
+    public class Customer
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string Phone { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string City { get; set; }
+
+        // navigation property
+        public virtual ICollection<Lease> Leases { get; set; }
+    }
+
+}
---
+++ InlandMarinaData/SlipsdbManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace InlandMarinaData
+{
+    public class SlipsdbManager
+    {
+        // Get all slips that are not leased
+        public static List<Slip> GetSlipsNotLeased(InlandMarinaContext db)
+        {
+            return db.Slips.Where(s => !s.Leases.Any()).ToList();
+        }
+
+        // Get slips filtered by dock ID and not leased
+        public static List<Slip> GetSlipsByDock(int dockId, InlandMarinaContext db)
+        {
+            return db.Slips.Where(s => s.DockID == dockId && !s.Leases.Any()).ToList();
+        }
+
+        // Get all slips (optional for CRUD operations)
+        public static List<Slip> GetAllSlips(InlandMarinaContext db)
+        {
+            return db.Slips.ToList();
+        }
+
+        // Other CRUD operations can be added here if needed (e.g., adding or deleting slips)
+    }
+}
